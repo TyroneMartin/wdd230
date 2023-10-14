8c63553f31fdd4874b0c34edd29ea9291758e156
Tyrone Martin <tyrone306@edu.com>
2023-10-13 19:53:18 -0600

    COC update

--- chamber/scripts/hamburgermenu.js
+++ chamber/scripts/hamburgermenu.js
@@ -1,20 +1,15 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const hamburgerMenu = document.getElementById("hamburger-menu");
-    const sidebarMenu = document.getElementById("sidebar-menu");
-    const overlay = document.querySelector(".overlay");
-    const menuVisible = false;
+const hidebutton = document.querySelector(".menu-hide");
+const showbutton = document.querySelector(".menu-show");
+const navmenu = document.querySelector("#nav-menu"); 
 
-    hamburgerMenu.addEventListener("click", function () {
-        if (!menuVisible) {
-            sidebarMenu.style.visibility = "visible";
-            sidebarMenu.style.left = "0";
-            overlay.style.visibility = "visible";
-            overlay.style.opacity = "0.4";
-        } else {
-            sidebarMenu.style.visibility = "hidden";
-            sidebarMenu.style.left = "-250px";
-            overlay.style.visibility = "hidden";
-            overlay.style.opacity = "0";
-        }
-    });
+hidebutton.addEventListener('click',() => {
+    hidebutton.classList.toggle('showing');
+    showbutton.classList.toggle('showing');
+    navmenu.classList.toggle('showing');
+});
+
+showbutton.addEventListener('click',() => {
+    hidebutton.classList.toggle('showing');
+    showbutton.classList.toggle('showing');
+    navmenu.classList.toggle('showing');        
 });
