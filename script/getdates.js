df9a0cc61753950d0f41ae82ca579e955e50f918
Tyrone Martin <tyrone306@byui.edu>
2023-10-28 02:01:02 -0600

    html clean up for COC

--- script/getdates.js
+++ script/getdates.js
@@ -1,5 +1,30 @@
-let year = new Date(); // Variable year is created to store the Date function 
-let presentYear = year.getFullYear(); // Retrive the year current year
-document.getElementById("currentYear").textContent = presentYear; // Output the curent year
-document.getElementById("lastUpdated").textContent = document.lastModified; //Output last modified using the lastModifued fuction 
+// Get the current year and display it
+let year = new Date(); 
+let presentYear = year.getFullYear(); 
+document.getElementById("currentYear").textContent = presentYear; 
+document.getElementById("currentYear1").textContent = presentYear; 
+document.getElementById("lastUpdated").textContent = document.lastModified; 
 
+function updateTime() {
+  // Get the current time
+  let currentTime = new Date();
+  let hours = currentTime.getHours();
+  let minutes = currentTime.getMinutes();
+  let seconds = currentTime.getSeconds();
+
+  // Convert 24-hour time format to 12-hour time format
+  let amPM = hours >= 12 ? 'PM' : 'AM';
+  hours = hours % 12 || 12; // The hour '0' should be '12'
+
+  // Format the time
+  let formattedTime = `${hours}:${minutes} ${amPM}`;
+
+  // Update the time in the DOM
+  document.getElementById("currentTime").textContent = formattedTime;
+}
+
+// Update time initially
+updateTime();
+
+// Update time every second (1000 milliseconds)
+setInterval(updateTime, 1000);
