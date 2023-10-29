4e3338cc1fed49981c219b86894191cab3b04cfd
Tyrone Martin <tyrone306@byui.edu>
2023-10-29 17:33:57 -0600

    update

--- chamber/scripts/discover.js
+++ chamber/scripts/discover.js
@@ -5,15 +5,15 @@ const DAY_IN_MILLIS = 24*60*60*1000;
 function getDiscoverMessage(){
     let message = "Welcome! Let us know if you have any questions."
     let lastVisitValue = localStorage.getItem(LAST_VISIT_DATE_KEY)
-    let today = new Date();
+    let today = new Date().getTime();
     if (lastVisitValue != null){
         lastVisit = parseInt(lastVisitValue)
-        daysSinceLastVisit = Math.floor((today.getTime() - lastVisit) / DAY_IN_MILLIS)
-        if (daysSinceLastVisit == 0){
+        daysSinceLastVisit = Math.floor((today - lastVisit) / DAY_IN_MILLIS)
+        if (daysSinceLastVisit === 0){
             message = "Back so soon? Awesome!"
         }
         else{
-            if (daysSinceLastVisit == 1){
+            if (daysSinceLastVisit === 1){
                 message = "You last visited 1 day ago."
             }
             else{
@@ -22,9 +22,13 @@ function getDiscoverMessage(){
             
         }
     }
-    localStorage.setItem(LAST_VISIT_DATE_KEY, `${today.getTime()}`)
+    else{
+        localStorage.setItem(LAST_VISIT_DATE_KEY, today);
+    }
+    localStorage.setItem(LAST_VISIT_DATE_KEY, today);
     return message;
 }
 
 // Set the message
-document.getElementById("message").textContent = getDiscoverMessage();
+let discoverMessage = getDiscoverMessage();
+document.getElementById("message").textContent = discoverMessage;
