f193d08ba420fedf8f50af616e9e1f1f0b7892aa
Tyrone Martin <tyrone306@edu.com>
2023-11-09 17:54:40 -0700

    Prophets API Call

--- lesson09/scripts/prophets.js
+++ lesson09/scripts/prophets.js
@@ -1,32 +1,54 @@
-// "name": "Joseph",
-// "lastname": "Smith",
-// "birthdate": "23 December 1805",
-// "death": "27 June 1844",
-// "length": 14,
-// "order": 1,
-// "birthplace": "Vermont",
-// "numofchildren": 11,
-// "imageurl": "https://asse
-
+// const url = "./data/prophets.json";
 const url = 'https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json';
+// COMPARE THIS TO THE VERSION FOUND IN THE W09 Activity: Working with JSON data and the Fetch API module
+// Using the innerHTML version is a little less Javascript intensive.
+const displayProphets = (prophets) => {
+  const cards = document.querySelector("div.cards"); // select the output container element
 
-const cards = document.querySelector('#cards');
+  prophets.forEach((prophet) => {
+    // Create elements to add to the div.cards element
+    let card = document.createElement("section");
+    // add the class card to the section
+    card.classList.add("card")
+    card.innerHTML = `<h2>${prophet.name} ${prophet.lastname}</h2>
+      <h3>Date of Birth: ${prophet.birthdate}</h3>
+      <h3>Place of Birth: ${prophet.birthplace}</h3>
+      <img src="${prophet.imageurl}" height="440" width="340" loading="lazy" alt="Portrait of ${prophet.name} ${prophet.lastname}">`;
+    cards.appendChild(card);
+  }); // end of forEach loop
+  
+}; // end of function expression
 
-function displayProphets(prophets) {
-    prophets.forEach ((element) => {
-        let section = '<h3>${prophet.name'
-        
+async function getProphetData() {
+  const response = await fetch(url);
+  if (response.ok) {
+    const data = await response.json();
+    displayProphets(data.prophets);
+  } else {
+    console.error("There was an error loading the data.");
+    const cards = document.querySelector("div.cards");
+    cards.innerHTML = "<section><h1>There was an error loading the data</h1></section>";
+  }
+}
+
+getProphetData();
+
+// This demonstrates another way to handle the fetch response using THEN instead of ASYNC/AWAIT
+
+function getProphetDataWithThen() {
+  fetch(url)
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        console.error("There was an error loading the data.");
+        const cards = document.querySelector("div.cards");
+        cards.innerHTML = "<section><h1>There was an error loading the data</h1></section>";
+      }
+    })
+    .then((data) => {
+      displayProphets(data.prophets);
     });
 }
 
-async function  getProphetData() {
-    const response = await fetch(url);
-    if (response.ok) {
-const data = await response.json
-displayProphets (data.Prophets);
-
-    }
-    else {
-        console.error("NO WORKIE!!!")
-    }
-}
+// getProphetDataWithThen();
