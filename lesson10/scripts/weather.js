f4486cc42e1b9720a9dc301227fa640d966aff5d
Tyrone Martin <tyrone306@edu.com>
2023-11-15 16:00:16 -0700

    update

--- lesson10/scripts/weather.js
+++ lesson10/scripts/weather.js
@@ -1,13 +1,15 @@
-const LAT = "18.471119";
-const LON = "-111.6777";
-const APIKEY = "1daaa26c0e994a2e7ac8523b60a91f1b";
-const apiURL = `https://api.openweathermap.org/data/2.5/weather?lat=${18.471119}&lon=${-77.987184}&appid=${1daaa26c0e994a2e7ac8523b60a91f1b}&units=imperial`;
-const apiURL = "../lesson10/scripts/weather.json";
+const LAT = "43.830043";
+const LON = "-111.8293824";
+const APIKEY = "1daaa26c0e994a2e7ac8523b60a91f1b";  // This is a free key, will be make public 
+// Choose either Option 1 or Option 2, not both
+const apiURL = `https://api.openweathermap.org/data/2.5/weather?lat=${43.830043}&lon=${-111.8293824}&appid=${1daaa26c0e994a2e7ac8523b60a91f1b}&units=imperial`;
+// const apiURL = "../lesson10/scripts/weather.json";
 
 function displayWeather(weatherData) {
+
   // You can use @2x or @4x to make the icon bigger, or omit it for the standard size
-  // const iconsrc = `https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`
-  const iconsrc = `https://openweathermap.org/img/wn/${weatherData.weather[0].icon}.png`
+  const iconsrc = `https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`
+  // const iconsrc = `https://openweathermap.org/img/wn/${weatherData.weather[0].icon}.png`
   const desc = weatherData.weather[0].description;
   const windSpeed = weatherData.wind.speed.toFixed(0);
   const temperature = weatherData.main.temp.toFixed(0);
@@ -20,7 +22,7 @@ function displayWeather(weatherData) {
   //Set up the weather description
   let weatherDesc = document.getElementById("weather-desc");
   weatherDesc.innerHTML = `${desc}`;
-  
+
   let weatherTemp = document.getElementById("weather-temp");
   weatherTemp.innerHTML = `${temperature}&deg;F | ${windSpeed} mph wind`;
 }
