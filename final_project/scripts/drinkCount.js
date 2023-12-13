25e5fe51a74410a57d9d7890f604ac1b7b7725eb
Tyrone Martin <tyrone306@byui.edu>
2023-12-13 07:40:45 -0700

    form update

--- final_project/scripts/drinkCount.js
+++ final_project/scripts/drinkCount.js
@@ -1,2 +1,9 @@
-var currentOrderCount = localStorage.getItem('orderCount') || 0;
-document.getElementById('total-Drink-Count').textContent = currentOrderCount;
+document.getElementById('submit-order').addEventListener('click', function() {
+    // Retrieve the current value from local storage (default to 0 if not set)
+    var currentOrderCount = localStorage.getItem('total-Drink-Count') || 0;
+
+    // Increment the value
+    currentOrderCount++;
+
+    localStorage.setItem('total-Drink-Count', currentOrderCount);
+});
