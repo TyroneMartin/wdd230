bf6e283c2e1e25235be8bf393c69ac65fdfd3ace
Tyrone Martin <tyrone306@byui.edu>
2023-12-13 21:12:51 -0700

    update

--- final_project/scripts/drinkCount.js
+++ final_project/scripts/drinkCount.js
@@ -1,2 +1,11 @@
+// Get the current order count from localStorage or default to 0
 var currentOrderCount = localStorage.getItem('orderCount') || 0;
-document.querySelector('.total-Drink-Count').textContent = currentOrderCount;
+
+// Update the HTML element with the current order count
+document.getElementById('total-Drink-Count').textContent = currentOrderCount;
+
+// Increment the order count
+currentOrderCount++;
+
+// Save the updated order count back to localStorage
+localStorage.setItem('orderCount', currentOrderCount);
